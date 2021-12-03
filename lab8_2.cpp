9199848105f8da8d6d02d3d8d38fac0663439602
Shuchanuch Rinkum <Suchanuch28@gmail.com>
2021-12-03 12:06:10 +0700

    lap8_2 completed

--- lab8_2.cpp
+++ lab8_2.cpp
@@ -5,25 +5,38 @@ using namespace std;
 
 int main()
 {
-string name, movie, day, text;
-int id, gear;
+
+int studentid,gear;
+string name,nmovie,day,answer;
+
 
 cout << "Fahsai: Sawadee ka...Can you tell me your name?\n?????: ";
+
 getline(cin, name);
 cout << "Fahsai: Wow!!! " << name << " is a really cool name.\n";
 cout << "Fahsai: I think you are an Engineering student. What is your student ID?\n" << name << ": ";
-cin >> id;
+
+cin >> studentid;
+
 cin.ignore();
-gear = ((id/10000000)-12);
+
+gear = ((studentid/10000000)-12);
 cout << "Fahsai: I think you may be GEAR " << gear << ". I have a free movie ticket for you.\n";
 cout << "Fahsai: Let's go to the cinema together!!!\n";
 cout << "Fahsai: What movie do you want to watch?\n";
 cout << name << ": ";
-getline(cin, movie);
+
+getline(cin, nmovie);
 cout << "Fahsai: So....which day are you free to go with me?\n" << name << ": ";
+
 getline(cin, day);
-cout << "Fahsai: " << day << "....that is OK!!! I'm looking forward to watching " << movie << " with you.\n" << name << ": ";
-getline(cin, text);
+cout << "Fahsai: " << day << "....that is OK!!! I'm looking forward to watching " << nmovie << " with you.\n" << name << ": ";
+
+getline(cin, answer);
 cout << "Fahsai: 555+ see you " << day << ". Bye Bye \\(^ ^)/";
+
+
 return 0;
+
+
 }
